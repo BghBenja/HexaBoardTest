adfc25db787483ff30ffd5269cb0b9504a5ce90c
BghBenja <bghbenja@gmail.com>
2023-01-10 17:33:49 +0100

    Start

---
+++ src/test/java/Board.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Board {
+
+    @Test
+    public void Hexa() {
+        Assertions.assertTrue(true);
+    }
+}
